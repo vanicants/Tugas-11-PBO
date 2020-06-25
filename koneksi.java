82aa5082eed45c9f322daf159c072b7ee1be016e
vani vabriana <61833895+vanicants@users.noreply.github.com>
2020-06-25 19:34:38 +0800

    Add files via upload

---
+++ koneksi.java
@@ -0,0 +1,31 @@
+package db_mhs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class koneksi {
+    static Connection koneksi = null;
+
+    public static Connection getConnection() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/mahasiswa";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Terkoneksi");
+            } catch (SQLException t) {
+                System.out.println("Error membuat koneksi");
+            }
+        }
+        return koneksi;
+    }
+    
+}
